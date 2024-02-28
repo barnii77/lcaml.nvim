6dd8fef7566307023a0244361fe16d23878e8231
barnii77 <david.harrer@gmx.at>
2024-02-28 19:16:53 +0100

    debug messages

--- lua/lcaml/init.lua
+++ lua/lcaml/init.lua
@@ -1,12 +1,14 @@
 local lcaml = {}
 
 function lcaml.init()
+  vim.notify("debug: lcaml.init")
   local callback = function(_) require("lcaml.syntax") end
   vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "BufNewFile" },
     { pattern = { "*.lml" }, callback = callback })
 end
 
 function lcaml.setup()
+  vim.notify("debug: lcaml.setup")
   lcaml.init()
 end
 
