5905bcb5b2fbcac157d48490079af307385da76f
barnii77 <david.harrer@gmx.at>
2024-03-01 17:03:32 +0100

    only apply syntax if the buffer is a .lml buffer

--- lua/lcaml/init.lua
+++ lua/lcaml/init.lua
@@ -4,40 +4,42 @@ local highlights = [[
 " Vim syntax file for Your Programming Language
 
 if !exists("b:current_syntax")
-  syntax clear
+  if expand("%:e") == "lml"
+    syntax clear
 
-  " Keywords
-  syntax keyword lcamlKeyword let return if else
-  syntax keyword lcamlStruct struct
-  syntax keyword lcamlType int float bool string list
-  syntax keyword lcamlTodo TODO
+    " Keywords
+    syntax keyword lcamlKeyword let return if else
+    syntax keyword lcamlStruct struct
+    syntax keyword lcamlType int float bool string list
+    syntax keyword lcamlTodo TODO
 
-  syntax match lcamlLiteral /()\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+.\d\+\)\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+\)/
-  syntax match lcamlBoolean /true\|false/
+    syntax match lcamlLiteral /()\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+.\d\+\)\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+\)/
+    syntax match lcamlBoolean /true\|false/
 
-  " syntax match lcamlIdentifier /\(\(true\|false\)\>\)\@!\([a-zA-Z_][a-zA-Z0-9_]*\)/
+    " syntax match lcamlIdentifier /\(\(true\|false\)\>\)\@!\([a-zA-Z_][a-zA-Z0-9_]*\)/
 
-  syntax match lcamlFunctionDef /|/
-  syntax match lcamlOperator /+\|-\|\*\|\/\|%\|==\|!=\|<\|<=\|>\|>=\|&&\|||\|!\|~/
+    syntax match lcamlFunctionDef /|/
+    syntax match lcamlOperator /+\|-\|\*\|\/\|%\|==\|!=\|<\|<=\|>\|>=\|&&\|||\|!\|~/
 
-  " Strings and Comments
-  syntax region lcamlString start=/"/ end=/"/ contains=@Spell
-  syntax match lcamlComment /--.*\n/ contains=@Spell
+    " Strings and Comments
+    syntax region lcamlString start=/"/ end=/"/ contains=@Spell
+    syntax match lcamlComment /--.*\n/ contains=@Spell
 
-  " Linking
-  highlight link lcamlKeyword Statement
-  highlight link lcamlType Type
-  highlight link lcamlStruct Structure
-  highlight link lcamlLiteral Constant
-  highlight link lcamlBoolean Boolean
-  " highlight link lcamlIdentifier Function  " Identifier
-  highlight link lcamlString String
-  highlight link lcamlComment Comment
-  highlight link lcamlFunctionDef Operator
-  highlight link lcamlOperator Operator
-  highlight link lcamlTodo Todo
+    " Linking
+    highlight link lcamlKeyword Statement
+    highlight link lcamlType Type
+    highlight link lcamlStruct Structure
+    highlight link lcamlLiteral Constant
+    highlight link lcamlBoolean Boolean
+    " highlight link lcamlIdentifier Function  " Identifier
+    highlight link lcamlString String
+    highlight link lcamlComment Comment
+    highlight link lcamlFunctionDef Operator
+    highlight link lcamlOperator Operator
+    highlight link lcamlTodo Todo
 
-  let b:current_syntax = "lml"
+    let b:current_syntax = "lml"
+  endif
 endif
 ]]
 
