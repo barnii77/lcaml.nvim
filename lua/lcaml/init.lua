1ea49f292a9558b85c4e4606bb55f9ba2998cd72
barnii77 <david.harrer@gmx.at>
2024-12-03 20:44:29 +0100

    fixing stuff

--- lua/lcaml/init.lua
+++ lua/lcaml/init.lua
@@ -13,8 +13,8 @@ if !exists("b:current_syntax")
     syntax keyword lcamlType int float bool string list
     syntax keyword lcamlTodo TODO
 
-    syntax match lcamlLiteral /()\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+.\d\+\)\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+\)/
     syntax match lcamlNumber /\d+(\.\d+)?/
+    syntax match lcamlLiteral /()\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+.\d\+\)\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+\)/
     syntax match lcamlBoolean /true\|false/
 
     " syntax match lcamlIdentifier /\(\(true\|false\)\>\)\@!\([a-zA-Z_][a-zA-Z0-9_]*\)/
@@ -30,8 +30,8 @@ if !exists("b:current_syntax")
     highlight link lcamlKeyword Statement
     highlight link lcamlType Type
     highlight link lcamlStruct Structure
-    highlight link lcamlLiteral Constant
     highlight link lcamlNumber Constant
+    highlight link lcamlLiteral Constant
     highlight link lcamlBoolean Boolean
     " highlight link lcamlIdentifier Function  " Identifier
     highlight link lcamlString String
