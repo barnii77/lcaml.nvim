8bc046c00c2b3416cd46a40e3088e991fa1f516f
barnii77 <david.harrer@gmx.at>
2024-12-06 12:14:26 +0100

    I really dont want to push every time I make changes but lazy only fetches new version if I do so... sry :)

--- lua/lcaml/init.lua
+++ lua/lcaml/init.lua
@@ -1,47 +1,45 @@
 local lcaml = {}
 
 local highlights = [[
-" Vim syntax file for Your Programming Language
+" Vim syntax file for the LCaml Programming Language
 
-if !exists("b:current_syntax")
-  if expand("%:e") == "lml"
-    " Keywords
-    syntax keyword lcamlKeyword let return if else while
-    syntax keyword lcamlStruct struct
-    syntax keyword lcamlType int float bool string list
-    syntax keyword lcamlJit jit
-    syntax keyword lcamlTodo TODO
+if expand("%:e") == "lml"
+  " Keywords
+  syntax keyword lcamlKeyword let return if else while
+  syntax keyword lcamlStruct struct
+  syntax keyword lcamlType int float bool string list
+  syntax keyword lcamlJit jit
+  syntax keyword lcamlTodo TODO
 
-    syntax match lcamlLiteral /()\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+.\d\+\)\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+\)/
-    syntax match lcamlNumber /\d\+\(\.\d\+\)?/
-    syntax match lcamlBoolean /true\|false/
+  syntax match lcamlLiteral /()\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+.\d\+\)\|\(\([0-9a-zA-Z_]\)\>\)\@!\(\d\+\)/
+  " syntax match lcamlNumber /\d\+\(\.\d\+\)?/
+  syntax match lcamlBoolean /true\|false/
 
-    " syntax match lcamlIdentifier /\(\(true\|false\)\>\)\@!\([a-zA-Z_][a-zA-Z0-9_]*\)/
+  " syntax match lcamlIdentifier /\(\(true\|false\)\>\)\@!\([a-zA-Z_][a-zA-Z0-9_]*\)/
 
-    syntax match lcamlFunctionDef /|/
-    syntax match lcamlOperator /+\|-\|\*\|\/\|%\|==\|!=\|<\|<=\|>\|>=\|&&\|||\|!\|~/
+  syntax match lcamlFunctionDef /|/
+  syntax match lcamlOperator /+\|-\|\*\|\/\|%\|==\|!=\|<\|<=\|>\|>=\|&&\|||\|!\|~/
 
-    " Strings and Comments
-    syntax region lcamlString start=/"/ end=/"/
-    syntax match lcamlComment /--.*\n/
+  " Strings and Comments
+  syntax region lcamlString start=/"/ end=/"/
+  syntax match lcamlComment /--.*\n/
 
-    " Linking
-    highlight link lcamlKeyword Statement
-    highlight link lcamlType Type
-    highlight link lcamlJit Type
-    highlight link lcamlStruct Structure
-    highlight link lcamlLiteral Constant
-    highlight link lcamlNumber Constant
-    highlight link lcamlBoolean Boolean
-    " highlight link lcamlIdentifier Function  " Identifier
-    highlight link lcamlString String
-    highlight link lcamlComment Comment
-    highlight link lcamlFunctionDef Operator
-    highlight link lcamlOperator Operator
-    highlight link lcamlTodo Todo
+  " Linking
+  highlight link lcamlKeyword Statement
+  highlight link lcamlType Type
+  highlight link lcamlJit Type
+  highlight link lcamlStruct Structure
+  highlight link lcamlLiteral Constant
+  " highlight link lcamlNumber Constant
+  highlight link lcamlBoolean Boolean
+  " highlight link lcamlIdentifier Function  " Identifier
+  highlight link lcamlString String
+  highlight link lcamlComment Comment
+  highlight link lcamlFunctionDef Operator
+  highlight link lcamlOperator Operator
+  highlight link lcamlTodo Todo
 
-    let b:current_syntax = "lml"
-  endif
+  let b:current_syntax = "lml"
 endif
 ]]
 
