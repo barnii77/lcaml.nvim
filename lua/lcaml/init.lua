244f75e8edc2d862759ae3d06eb10e5421a84627
barnii77 <david.harrer@gmx.at>
2024-12-06 12:44:41 +0100

    I really dont want to push every time I make changes but lazy only fetches new version if I do so... sry :)

--- lua/lcaml/init.lua
+++ lua/lcaml/init.lua
@@ -4,7 +4,7 @@ local highlights = [[
 " Vim syntax file for the LCaml Programming Language
 
 if expand("%:e") == "lml"
-  echoerr "lcaml syntax loading"
+  echomsg "lcaml syntax loading"
   " Keywords
   syntax keyword lcamlKeyword let return if else while
   syntax keyword lcamlStruct struct
@@ -22,7 +22,7 @@ if expand("%:e") == "lml"
   " Strings and Comments
   syntax region lcamlString start=/"/ end=/"/
   syntax match lcamlComment /--.*\n/
-  echoerr "gonna link now"
+  echomsg "gonna link now"
 
   " Linking
   highlight link lcamlKeyword Statement
